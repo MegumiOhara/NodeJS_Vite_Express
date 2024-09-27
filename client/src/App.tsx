1cdd2a98df558751674bf6cc175e5fb174d15bc1
MegumiOhara <156937802+MegumiOhara@users.noreply.github.com>
2024-09-27 21:42:49 +0200

    installed axios, send data to frontend
    install cos

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,21 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import axios from 'axios'
 import './App.css'
+
 
 function App() {
   const [count, setCount] = useState(0)
+
+  const fetchApi = async () => {
+    const response = await axios.get("http://localhost:8080/api");
+    console.log(response);
+  };
+
+  useEffect(() => {
+    fetchApi();
+  }, []);
 
   return (
     <>
